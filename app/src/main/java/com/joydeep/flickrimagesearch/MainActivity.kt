59c965c8ed9e262b9b5afe91a0bc0ac2f634d833
JOYDEEP DAS <joydeep.das@olx.com>
2021-07-09 02:28:26 +0530

    Initial commit

---
+++ app/src/main/java/com/joydeep/flickrimagesearch/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.joydeep.flickrimagesearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
